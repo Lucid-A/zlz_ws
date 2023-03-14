ab11c1a6e277cd95b17f5b7ae7c514b65d17ba87
board2 <nx@board2>
2023-03-14 23:00:54 +0800

    To be tested: make some nodes anonymous and fix typo; Formatted rrt_exploration

--- src/map_merge/scripts/forward_map.py
+++ src/map_merge/scripts/forward_map.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python
 import os
 import rospy
-import nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import OccupancyGrid
 
 class ForwardMap:
     def __init__(self, name : str):
-        self.sub = rospy.Subscriber("map", OccupancyGrid, callback, queue_size=1000)
+        self.sub = rospy.Subscriber("map", OccupancyGrid, self.callback, queue_size=1000)
         self.pub = rospy.Publisher(f"/{name}/submap", OccupancyGrid, queue_size=1000)
         rospy.spin()
     def callback(self, map_msg):
